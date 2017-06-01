79dd6155abe3e3e2c1619d8e896525732ed8d43a
dissasta <kapuscikjan@gmail.com>
2017-06-01 15:01:27 +0100

    drobne zmiany w constantach dodane klasy

--- constants.py
+++ constants.py
@@ -4,13 +4,9 @@ screenHeight = 1080
 
 screenSize = (screenWidth, screenHeight)
 
-tileSize = screenWidth / 48
-mapWidth = screenWidth / tileSize
-mapHeight = screenHeight / tileSize
+tileSize = 40
+mapWidth = 60
+mapHeight = 60
 
-playerPostionX = 2
-playerPostionY = 2
-targetPostionX = mapWidth - 8
-targetPostionY = mapHeight - 8
+
 playerColour = (255,255,255)
-targetColour = (30,190,200)
--- main.py
+++ main.py
@@ -4,41 +4,68 @@ import time
 from random import randint
 
 class Tile:
-    def __init__(self, block_path):
+    def __init__(self, block_path, posX, posY):
+        self.colours = ((40, 40, 40), (100, 100, 100), (0, 0, 0))
         self.block_path = block_path
-        self.colour = self.pickColour()
+        self.colour = self.colours[2]
+        self.uncovered = False
+        self.obscuring = False
+        self.sizeX = 40
+        self.sizeY = 40
+        self.positionX = posX
+        self.positionY = posY
 
-    def pickColour(self):
-        if self.block_path:
-            return (0,0,0)
+    def update(self):
+        if not self.uncovered:
+            self.colour = self.colours[2]
         else:
-            return (100,100,100)
+            if self.block_path:
+                self.colour = self.colours[0]
+            else:
+                self.colour = self.colours[1]
 
-class Game:
-    def __init__(self):
-        self.running = True
-        self.size = constants.screenSize
-        self.FPS = 60
-        self.clock = pygame.time.Clock()
+    def render(self, screen):
+        pygame.draw.rect(screen, self.colour, (self.positionX * self.sizeX, self.positionY * self.sizeY, self.sizeX, self.sizeY))
 
-    def on_init(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self.map = self.map_create()
-        self.running = True
+class Player(Tile):
+    def __init__(self, posX, posY):
+        Tile.__init__(self, True, posX, posY)
+        self.colour = (255,255,255)
+        self.vision = 5
+        self.orientation = None
 
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self.running = False
+    def update(self, map):
+        count = 1
+        visible = []
 
-    def on_cleanup(self):
-        pygame.quit()
+        #north
+        while count != self.vision:
+            for y in range(self.vision):
+                for x in range(self.positionX - count, self.positionX + count + 1):
+                    visible.append((x, self.positionY - y))
+            count += 1
 
-    def findPath(self):
+        for tile in visible:
+            count = self.vision
+
+            #if not map[tile[0]][tile[1] - 1].block_path:
+            map[tile[0]][tile[1]].uncovered = True
+
+        print visible
+
+class Monster(Tile):
+    def __init__(self, posX, posY):
+        Tile.__init__(self, True, posX, posY)
+        self.colour = (136,200,55)
+
+    def update(self):
+        pass
+
+    def findPath(self, map):
         found = False
         step = 0
-        startTile = (constants.playerPostionX, constants.playerPostionY)
-        target = (constants.targetPostionX, constants.targetPostionY)
+        startTile = (self.positionX, self.positionY)
+        target = (hero.positionX, hero.positionY)
         toCheck = [startTile]
         checked = []
         path = []
@@ -51,65 +78,92 @@ class Game:
                 westTile = (tile[0] - 1, tile[1])
                 eastTile = (tile[0] + 1, tile[1])
 
-                if not self.map[tile[0]-1][tile[1]].block_path and westTile not in checked and westTile not in toCheck:
+                if not map[tile[0]-1][tile[1]].block_path and westTile not in checked and westTile not in toCheck:
                     toCheck.append(westTile)
 
-                if not self.map[tile[0]+1][tile[1]].block_path and eastTile not in checked and eastTile not in toCheck:
+                if not map[tile[0]+1][tile[1]].block_path and eastTile not in checked and eastTile not in toCheck:
                     toCheck.append(eastTile)
 
-                if not self.map[tile[0]][tile[1]-1].block_path and northTile not in checked and northTile not in toCheck:
+                if not map[tile[0]][tile[1]-1].block_path and northTile not in checked and northTile not in toCheck:
                     toCheck.append(northTile)
 
-                if not self.map[tile[0]][tile[1]+1].block_path and southTile not in checked and southTile not in toCheck:
+                if not map[tile[0]][tile[1]+1].block_path and southTile not in checked and southTile not in toCheck:
                     toCheck.append(southTile)
 
                 checked.append(tile)
                 path.append((tile[0], tile[1], step))
 
-                if tile == target:
+                if hero.positionX == tile[0] and hero.positionY == tile[1]:
                     found = True
                     break
 
             step += 1
 
         step -= 1
-        temp = 0
         while step != 1:
             nextTile = [x for x in path if x[2] == step - 1 and ((x[1] == target[1] and abs(x[0] - target[0]) == 1) or (x[0] == target[0] and abs(x[1] - target[1]) == 1))]
             randTile = nextTile[randint(0, len(nextTile) - 1)]
             target = randTile
-            self.map[target[0]][target[1]].colour = (200,100,100)
+            map[target[0]][target[1]].colour = (200,100,100)
             step -= 1
-            temp += 0
 
+class Game:
+    def __init__(self):
+        self.running = True
+        self.size = constants.screenSize
+        self.FPS = 60
+        self.clock = pygame.time.Clock()
+
+    def on_init(self):
+        global hero, monster
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.map = self.map_create()
+        hero = Player(7, 7)
+        monster = Monster(30, 20)
+        self.running = True
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self.running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                if not self.map[hero.positionX - 1][hero.positionY].block_path:
+                    hero.positionX -= 1
+                    hero.orientation = 'W'
+            elif event.key == pygame.K_RIGHT:
+                if not self.map[hero.positionX + 1][hero.positionY].block_path:
+                    hero.positionX += 1
+                    hero.orientation = 'E'
+            elif event.key == pygame.K_UP:
+                if not self.map[hero.positionX][hero.positionY - 1].block_path:
+                    hero.positionY -= 1
+                    hero.orientation = 'N'
+            elif event.key == pygame.K_DOWN:
+                if not self.map[hero.positionX][hero.positionY + 1].block_path:
+                    hero.positionY += 1
+                    hero.orientation = 'S'
+
+    def on_cleanup(self):
+        pygame.quit()
 
     def map_create(self):
-        new_map = [[Tile(False) for y in range(0,constants.mapHeight)] for x in range(0, constants.mapWidth)]
+        new_map = [[Tile(False, x, y) for y in range(0,constants.mapHeight)] for x in range(0, constants.mapWidth)]
         for x in range(0,constants.mapWidth):
             new_map[x][0].block_path = True
-            new_map[x][0].colour = (0,0,0)
 
         for x in range(0,constants.mapWidth):
             new_map[x][constants.mapHeight - 1].block_path = True
-            new_map[x][constants.mapHeight - 1].colour = (0,0,0)
 
-        for x in range(1,30):
-            new_map[x][constants.mapHeight -10].block_path = True
-            new_map[x][constants.mapHeight -10].colour = (0,0,0)
+        for x in range(1,40):
+            new_map[x][constants.mapHeight - 45].block_path = True
+            new_map[x][constants.mapHeight - 45].obscuring = True
 
         for y in range(0,constants.mapHeight):
             new_map[0][y].block_path = True
-            new_map[0][y].colour = (0,0,0)
 
         for y in range(0,constants.mapHeight):
             new_map[constants.mapWidth - 1][y].block_path = True
-            new_map[constants.mapWidth - 1][y].colour = (0,0,0)
-
-        new_map[constants.playerPostionX][constants.playerPostionY].player = True
-        new_map[constants.playerPostionX][constants.playerPostionY].colour = constants.playerColour
-
-        new_map[constants.targetPostionX][constants.targetPostionY].traget = True
-        new_map[constants.targetPostionX][constants.targetPostionY].colour = constants.targetColour
 
         return new_map
 
@@ -117,17 +171,24 @@ class Game:
         self.screen.fill((255,255,255))
 
     def on_render(self):
+
+        #for x in range(0, constants.mapWidth):
+        #    for y in range(0, constants.mapHeight):
+        #        if self.map[x][y].block_path == False:
+        #            self.map[x][y].colour = (100,100,100)
+        #monster.findPath(self.map)
         for x in range(0, constants.mapWidth):
             for y in range(0, constants.mapHeight):
-                    pygame.draw.rect(self.screen, (self.map[x][y].colour), (x * constants.tileSize,y * constants.tileSize, constants.tileSize, constants.tileSize))
-
+                    self.map[x][y].update()
+                    self.map[x][y].render(self.screen)
+        hero.update(self.map)
+        hero.render(self.screen)
+        monster.render(self.screen)
         pygame.display.update()
 
     def execute(self):
         if self.on_init() == False:
             self.running = False
-
-        self.findPath()
 
         while self.running:
 
@@ -135,8 +196,10 @@ class Game:
 
             for event in pygame.event.get():
                 self.on_event(event)
+
             self.on_loop()
             self.on_render()
+
         self.on_cleanup()
 
 if __name__ == "__main__":
